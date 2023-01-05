74fda6269a9c66a86bf5b816fe165b1ccb4bfcd7
yamake <yamake@gmail.com>
2023-01-06 00:19:35 +0900

    first commit

---
+++ main/src/main.rs
@@ -0,0 +1,18 @@
+use itertools::*;
+use num::*;
+use proconio::{
+    input,
+    marker::{Bytes, Chars, Usize1},
+};
+use std::collections::BTreeSet;
+use std::collections::HashSet;
+
+const inf: i32 = 1000000500;
+const linf: i64 = 1223372036854775807;
+
+fn main() {
+    let a = 0;
+    for i in 0..10 {
+        println!("{}", i);
+    }
+}
