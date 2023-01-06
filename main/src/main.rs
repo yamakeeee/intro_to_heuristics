6e9a9121c223a6aea5ae45df900ec8c5f816e3bd
yamake <yamake@gmail.com>
2023-01-06 12:32:41 +0900

    feat: add calc score

--- main/src/main.rs
+++ main/src/main.rs
@@ -7,12 +7,35 @@ use proconio::{
 use std::collections::BTreeSet;
 use std::collections::HashSet;
 
-const inf: i32 = 1000000500;
-const linf: i64 = 1223372036854775807;
+const UINF: usize = 2000000500;
+const INF: i32 = 1000000500;
+const LINF: i64 = 1223372036854775807;
+
+const M: usize = 26;
+const D: usize = 365;
+
+fn calc_score(c: &[i32], s: &[Vec<i32>], t: &[i32]) {
+    let mut res = 0;
+    let mut last = vec![0; M];
+    for i in 0..D {
+        last[t[i] as usize] = i + 1;
+        res += s[i][t[i] as usize];
+        for j in 0..M {
+            res -= (i as i32 + 1 - last[j] as i32) * c[j];
+        }
+        println!("{}", res);
+    }
+}
 
 fn main() {
-    let a = 0;
-    for i in 0..10 {
-        println!("{}", i);
+    input! {
+        _d:usize,
+        c:[i32;M],
+        s:[[i32;M];D],
+        mut t:[i32;D],
     }
+    for i in 0..D {
+        t[i] -= 1;
+    }
+    calc_score(&c, &s, &t);
 }
